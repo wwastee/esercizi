c52e627bd9af21caf33873a187fca50a34b321c8
francesco_bertolini <fraberto001@gmail.com>
2022-09-19 15:57:40 +0200

    orinde del 5

--- esercizio5.js
+++ esercizio5.js
@@ -1,15 +1,14 @@
 const prompt = require('prompt-sync')();
 
-let NumeroImput = prompt("Inserisci un numero e ti mostrerò un trucco di magia: ");
+let numeroImput = prompt("Inserisci un numero e ti mostrerò un trucco di magia: ");
 
 let somma = 0
 
-
-for(let i=0; i <= NumeroImput; i++){
+for(let i=0; i <= numeroImput; i++){
     if(i % 3 == 0 && i % 7 ==0){     //punto bonus
-    somma=somma + i
-    console.log(i + ": " + somma)
-};
+        somma=somma + i
+        console.log(i + ": " + somma)
+    };
 };
 
 console.log(somma);
---
+++ eserizio7.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt-sync')();
+
+somma= 0
+
+for(let i=0; i<numeroInput; i++){
+    i=i
+}
