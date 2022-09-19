e90b51552fe1b8187392cc9dc1e0088c352d35a8
francesco_bertolini <fraberto001@gmail.com>
2022-09-19 14:57:12 +0200

    commit esercizio 4(da sistemare) e esercizio 5 con bonus

--- esercizio5.js
+++ esercizio5.js
@@ -4,21 +4,12 @@ let NumeroImput = prompt("Inserisci un numero e ti mostrerò un trucco di magia:
 
 let somma = 0
 
+
 for(let i=0; i <= NumeroImput; i++){
+    if(i % 3 == 0 && i % 7 ==0){     //punto bonus
     somma=somma + i
     console.log(i + ": " + somma)
 };
-
-console.log(somma);
-
-
-let moltiplicazione = 1
-
-for(let i=1; i <= NumeroImput; i++){
-    moltiplicazione=moltiplicazione * i
-    console.log(i + ": " + moltiplicazione)
 };
 
-console.log(moltiplicazione);
-
-console.log("TA DAN ma vai a fanculo");
+console.log(somma);
